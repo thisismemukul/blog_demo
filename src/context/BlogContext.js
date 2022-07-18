608b3e82338e61d6487d83310eb478ea76a5a1f0
Mukul Saini <thisismemukul@gmail.com>
2022-07-18 17:37:36 +0530

    Delete Blog Post

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -3,7 +3,9 @@ import createDataContext from './createDataContext';
 const blogReducer = (state, action) => {
     switch (action.type) {
         case 'ADD_BLOG_POST':
-            return [...state, { title: `Blog Post #${state.length + 1}`, description: action.payload }];
+            return [...state, { id:Math.floor(Math.random()*99999), title: `Blog Post #${state.length + 1}`, description: action.payload }];
+        case 'DELETE_BLOG_POST':
+            return state.filter((blogPost) => blogPost.id !== action.payload);
         default:
             return state;
     }
@@ -14,6 +16,11 @@ const addBlogPost = (dispatch) => {
         dispatch({ type: 'ADD_BLOG_POST', payload: content });
     }
 }
+const deleteBlogPost = (dispatch) => {
+    return (id) => {
+        dispatch({ type: 'DELETE_BLOG_POST', payload: id });
+    }
+}
 
 
-export const { Context, Provider } = createDataContext(blogReducer, { addBlogPost }, []);
+export const { Context, Provider } = createDataContext(blogReducer, { addBlogPost,deleteBlogPost }, []);
--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,24 +1,31 @@
-import { StyleSheet, Text, View, FlatList, Button } from 'react-native'
+import { StyleSheet, Text, View, FlatList, Button, TouchableOpacity } from 'react-native'
 import React, { useContext } from 'react'
-import {Context} from '../context/BlogContext'
+import { Context } from '../context/BlogContext';
+import { Ionicons } from '@expo/vector-icons';
+
 const IndexScreen = () => {
-    const { state, addBlogPost } = useContext(Context);
+    const { state, addBlogPost, deleteBlogPost } = useContext(Context);
     return (
         <View style={styles.container}>
-            <Text>IndexScreen</Text>
-            <Button title="Add Post" onPress={() => addBlogPost('New Post')} />
+            <Button title="Add Post" onPress={() => addBlogPost('NewNewNewNewNewNewNewNewNewNewNewNewNewNewNewNewNewNewNewNewNewNewNewNewNewNewNewNewNewNewNewNewNewNewNewNewNewNewNewNewNewNewNewNewNewNewNewNewNewNewNew Post')} />
             <FlatList
                 data={state}
-                keyExtractor={(blogPost) => blogPost.title.toString()}
+                keyExtractor={(blogPost) => blogPost.id.toString()}
                 renderItem={({ item }) => {
-                    return <Text style={styles.text}>{item.title} - {item.description}</Text>
+                    return (
+                        <View style={styles.listItem}>
+                            <View style={styles.listItemView}>
+                                <Text style={styles.title}>{item.title}</Text>
+                                <Text style={styles.description}>{item.description}</Text>
+                            </View>
+                            <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
+                                <Ionicons style={styles.icon} name="trash-bin" size={24} color="black" />
+                            </TouchableOpacity>
+                        </View>
+                    )
                 }
                 }
             />
-
-            {/* {blogPosts.map(blogPost => {
-                return <Text key={blogPost.id}>{blogPost.title}</Text>
-            })} */}
         </View>
     )
 }
@@ -29,9 +36,32 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#fff',
-        alignItems: 'center',
         justifyContent: 'center',
-        marginHorizontal: 20,
-        marginVertical: 50
+        paddingHorizontal: 20,
+        paddingVertical: 50
     },
+    listItem: {
+        backgroundColor: '#eee',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: 10,
+        borderColor: 'black',
+        borderWidth: 1,
+        marginVertical: 10
+    },
+    listItemView: {
+        flex: 11,
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        alignItems: 'flex-start'
+    },
+
+    text: {
+        fontSize: 18
+    },
+    icon: {
+        flex: 1,
+    }
+
 });
